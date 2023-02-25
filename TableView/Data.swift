98623d827c4f536f39ee36fa4092b614fd8e656e
Carrington <Manyuchi>
2023-02-25 13:16:35 +0200

    tableView with Sections

---
+++ TableView/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  TableView
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/02/25.
+//
+
+import Foundation
+
+
+struct TableData {
+    
+    let sectionType: String?
+    let sectionName: [String?]
+}
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var data = [
+        TableData(sectionType: "Animals", sectionName: ["Rabbit", "Cat", "Horse", "Dog"]),
+        TableData(sectionType: "Electronics", sectionName: ["TV", "Radio", "Computer", "Heater"]),
+        TableData(sectionType: "Vegetables", sectionName: ["Carrots", "Onions","Chillies","Tomato"])
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +26,31 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return data[section].sectionType
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].sectionName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = data[indexPath.section].sectionName[indexPath.row]
+        return cell!
+    }
+    
+    
+    
+    
+   
+    
+    
+}
